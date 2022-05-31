788a7ed23fc42203c0fae0b0205f0aa8591f53d3
ngseiyu <44496936+ngseiyu@users.noreply.github.com>
2022-05-31 21:24:26 +0800

    finished coding the api

--- index.js
+++ index.js
@@ -5,28 +5,48 @@
 var express = require('express');
 var app = express();
 
+app.use(express.json());
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+    res.json({ greeting: 'hello API' });
 });
 
-
+// answer to the problem
+app.get("/api/:date", (req, res) => {
+    const { date } = req.params;
+    let year, month, day, d;
+    if (date.includes("-")) {
+        [year, month, day] = date.match(/[0-9]+/g);
+        d = new Date(Date.UTC(year, month - 1, day));
+    } else {
+        d = new Date(Number(date));
+    }
+    const result = {
+        unix: d.getTime(),
+        utc: d.toUTCString()
+    }
+    res.send(result);
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log('Listening on port ' + listener.address().port);
 });
+/* app.listen(8080, () => {
+    console.log(`Listening on port 8080`);
+}); */
